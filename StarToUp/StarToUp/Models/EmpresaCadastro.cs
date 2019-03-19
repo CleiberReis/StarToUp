11a9960126e6714cd6ea068a598ebddc02dec637
Gabriele Kiane <39500774+gabrielekiane@users.noreply.github.com>
2019-03-19 01:33:05 -0300

    Algumas alterações no layout da parte de cadastro da empresa.

--- StarToUp/StarToUp/Models/EmpresaCadastro.cs
+++ StarToUp/StarToUp/Models/EmpresaCadastro.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,16 @@ namespace StarToUp.Models
 {
     public class EmpresaCadastro
     {
+        //[Key]
+        //public int EmpresaID { get; set; }
+        //[Required(ErrorMessage = "Preencha a descrição da Empresa")]
+        //[DisplayName("Descrição")]
+        //[StringLength(50, MinimumLength = 3, ErrorMessage = "A descrição do estado deve ter entre 3 e 50 caracteres.")]
+        //public string Descricao { get; set; }
+        //[Required(ErrorMessage = "Preencha a sigla do estado")]
+        //[StringLength(2, MinimumLength = 2, ErrorMessage = "A sigla do estado deve ter 2 caracteres.")]
+
+      
         public int EmpresaCadastroID { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
