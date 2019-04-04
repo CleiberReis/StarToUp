7ad1344a8c0fe0d3b3145103cb224fa3e6464a21
DesireeFraga95 <fragadeh.a@gmail.com>
2019-04-04 00:30:07 -0300

    Tentativa de getbyid falha

--- StarToUp/StarToUp/Repositories/Funcoes.cs
+++ StarToUp/StarToUp/Repositories/Funcoes.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
@@ -87,6 +89,8 @@ namespace StarToUp.Repositories
             //HttpContext.Current.Response.Cookies["Usuario"].Value = "";
             FormsAuthentication.SignOut();
         }
+
+       
     }
-    
+
 }
