ac4c493c4216b7e684e0b5e170982b4f6f092f10
DesireeFraga95 <fragadeh.a@gmail.com>
2019-04-07 20:08:33 -0300

    Links de redirecionamenro para home Principal ok

--- StarToUp/StarToUp/Controllers/PerfilEmpresasController.cs
+++ StarToUp/StarToUp/Controllers/PerfilEmpresasController.cs
@@ -42,41 +42,7 @@ namespace StarToUp.Controllers
         //public ActionResult Logoff()
         //{
         //    StarToUp.Repositories.Funcoes.Deslogar();
-        //    return RedirectToAction("Index", "Home");
-        //}
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Login(StartupCadastro u)
-        //{
-        //    // esta action trata o post (login)
-        //    if (ModelState.IsValid) //verifica se é válido
-        //    {
-        //        using (StartupCadastrosController dc = new StartupCadastrosController())
-        //        {
-        //            var v = dc.StartupCadastros.Where(a => a.NomeUsuario.Equals(u.Nome) && a.Senha.Equals(u.Senha)).FirstOrDefault();
-        //            if (v != null)
-        //            {
-        //                Session["StartupCadastroID"] = v.Id.ToString();
-        //                Session["nome"] = v.NomeUsuario.ToString();
-        //                return RedirectToAction("Index", "IndexStartup");
-        //            }
-        //        }
-        //    }
-        //    return View(u);
-        //}
-
-        //public ActionResult About()
-        //{
-        //    ViewBag.Message = "Your application description page.";
-
-        //    return View();
-        //}
-
-        //public ActionResult Contact()
-        //{
-        //    ViewBag.Message = "Your contact page.";
-
-        //    return View();
+        //    return RedirectToAction("Index", "Principal");
         //}
 
         // GET: PerfilEmpresas/Details/5
