8d6447d8b3d24eb370a4a12218e65ed5bf73db56
DesireeFraga95 <fragadeh.a@gmail.com>
2019-04-23 22:12:18 -0300

    Alterações

--- StarToUp/StarToUp/Controllers/StartupCadastrosController.cs
+++ StarToUp/StarToUp/Controllers/StartupCadastrosController.cs
@@ -47,32 +47,7 @@ namespace StarToUp.Controllers
             return View();
         }
 
-        public ActionResult EsqueciSenha(int? id)
-        {
-            var startupCadastros = db.StartupCadastros;
-            return View(startupCadastros.ToList());
-            //return View();
-        }
-
-        [HttpPost]
-        public ActionResult EsqueciSenha([Bind(Include = "StartupCadastroID,Nome,Email,Senha,Cep,Rua,Bairro,Numero,Complemento,Cidade,Estado,Sobre,Objetivo,DataFundacao,TamanhoTime,Logotipo,ImagemLocal1,ImagemLocal2,ImagemMVP1,ImagemMVP2,ImagemMVP3,ImagemMVP4,SegmentacaoID")] StartupCadastro startupCadastro,
-            HttpPostedFileBase logoTipo, HttpPostedFileBase imagemLocal1, HttpPostedFileBase imagemLocal2, HttpPostedFileBase imagemMVP1, HttpPostedFileBase imagemMVP2, HttpPostedFileBase imagemMVP3, HttpPostedFileBase imagemMVP4)
-        {
-            Funcoes.GetUsuario();
-            var cons = db.StartupCadastros.Where(c => c.Email == startupCadastro.Email).Where(c => c.Senha == startupCadastro.Senha);
-
-            GmailEmailService gmail = new GmailEmailService();
-            EmailMessage msg = new EmailMessage();
-            msg.Body = "<!DOCTYPE HTML><html><body><p>" + startupCadastro.Nome + ",<br/>Olá!</p><p>Segue aqui a sua última senha cadastrada: <br/>" + startupCadastro.Senha + "</p><p>Aconselhamos que por segurança você altere sua senha!</p><p>Clique no link abaixo para logar novamente:</p><a href= http://localhost:50072/Logon/Logar/" + "><p>Atenciosamente,<br/>StarToUp.</p></body></html>";
-            msg.IsHtml = true;
-            msg.Subject = "Esqueceu a senha? Abra o E-mail - StarToUp";
-            msg.ToEmail = startupCadastro.Email;
-            gmail.SendEmailMessage(msg);
-
-            return View();
-
-        }
-
+       
         public ActionResult Logoff()
         {
             StarToUp.Repositories.Funcoes.Deslogar();
